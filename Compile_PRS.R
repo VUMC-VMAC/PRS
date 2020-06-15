8fcdf7bdbbcba0e6e1d4b6c606c724f918a888c8
ermahoney18 <emily.mahoney@vumc.org>
2020-06-15 15:44:07 -0500

    Adding in APOE exclusion option and updating permissions

--- Compile_PRS.R
+++ Compile_PRS.R
@@ -4,6 +4,7 @@ genotypes_stem <- args[2]
 output_tags <- args[3]
 pvalues <- args[4]
 output_stem <- args[5]
+apoe_exclude <- args[6]
 
 library(data.table)
 
@@ -11,10 +12,13 @@ library(data.table)
 output_tags <- unlist(strsplit(output_tags, ","))
 pvalues <- unlist(strsplit(pvalues, ","))
 
+#deal with the APOE argument
+apoe <- if(apoe_exclude == "yes"){ apoe <-  c("_noAPOE", "") } else { apoe <- "" }
+
 ### create list of possible profile (PRS) files ###
 #use expand grid to get every possible combination (pasting additional values with genotypes_stem and pvalues to be able to paste the file name together)
-PRS_files <- expand.grid(output_folder, paste0(genotypes_stem, "_"), output_tags, paste0("_PRS.Pval_", pvalues, ".profile"), stringsAsFactors = F)
-names(PRS_files) <- c("output_folder", "genotypes_stem", "output_tags", "pvalues")
+PRS_files <- expand.grid(output_folder, paste0(genotypes_stem, "_"), output_tags, apoe, paste0("_PRS.Pval_", pvalues, ".profile"), stringsAsFactors = F)
+names(PRS_files) <- c("output_folder", "genotypes_stem", "output_tags", "apoe", "pvalues")
 #paste the file names together
 PRS_files$filename <- apply(PRS_files, 1, paste0, collapse="")
 #remove additional text from pvalues
@@ -29,7 +33,7 @@ scores <- lapply(PRS_files$filename, fread)
 #combine into one df
 ids <- scores[[1]][,c("FID", "IID")]
 scores <- cbind(ids, sapply(scores, "[[", 6))
-names(scores) <- c("FID", "IID", paste(PRS_files$output_tags, PRS_files$pvalues, sep = "_"))
+names(scores) <- c("FID", "IID", paste0(PRS_files$output_tags, PRS_files$apoe, "_", PRS_files$pvalues))
 
 #write out
 write.table(scores, paste0(output_folder, "/", output_stem, ".txt"), col.names = T, row.names = F, sep = " ", quote = F)
--- Generate_PRS.sh
+++ Generate_PRS.sh
@@ -3,14 +3,40 @@
 #fail on error
 set -e
 
-#assumptions at the beginning of this script:
-# the summary stats are already in shape to be able to figure out overlap
-# will still need to determine the overlapping variants
-
 ############################# Verify inputs ############################
 
+#define usage
+display_usage() {
+    printf "Polygenic Risk Score Calculation Script
+
+Usage:
+Generate_PRS.sh -i GENOTYPES_STEM -s SUMMARY_STATISTICS -f OUTPUT_FOLDER -t OUTPUT_TAGS -o OUTPUT_STEM -p PVALUE_THRESHOLDS -r R2_THRESHOLD -w CLUMPING_WINDOW -b GENOME_BUILD -a
+
+GENOTYPES_STEM = the full path and stem to the genotypes for individuals to calculate the risk scores in (must be in plink binary format)
+SUMMARY_STATISTICS = the full path and file name for the summary statistics to calculate the risk scores from (must have the columns SNP, A1, BETA, and P, but can jave other columns as well); note that you can specify multiple summary statistics files here separated by a comma (no space)
+OUTPUT_FOLDER = the folder in which to calculate the PRS
+OUTPUT_TAGS = a tag for each summary statistics file supplied (comma separated with no space); will be in the variable name in the outputted file with all the PRS
+OUTPUT_STEM = name for the output file of all the PRS calculated
+PVALUE_THRESHOLDS = comma separated p value thresholds for variants to be included in PRS calculation; default is plink's default of 0.0001 (optional)
+R2_THRESHOLD = r-squared threshold for clumping; default is plink's default of 0.5 (optional)
+CLUMPING_WINDOW = window in which to conduct the clumping; default is plink's default of 250 (optional)
+GENOME_BUILD = the genome build for the input genotypes; only used if the -a flag is given; default is b38 (optional)
+
+-a indicates to exclude the APOE region from PRS calculation
+
+-h will show this usage
+"
+        }
+
+#set defaults (will be written over if the argument was supplied)
+pvalues=0.0001
+r2thresh=0.5
+window=250
+apoe_exclude=no
+genome_build=b38
+
 #parse arguments
-while getopts 'i:s:f:t:o:p:r:w:h' flag; do
+while getopts 'i:s:f:t:o:p:r:w:b:ah' flag; do
   case "${flag}" in
     i) genotypes="${OPTARG}" ;;
     s) sumstats="${OPTARG}" ;;
@@ -20,15 +46,13 @@ while getopts 'i:s:f:t:o:p:r:w:h' flag; do
     p) pvalues="${OPTARG}" ;;
     r) r2thresh="${OPTARG}" ;;
     w) window="${OPTARG}" ;;
+    b) genome_build="${OPTARG}" ;;
+    a) apoe_exclude=yes ;;
     h) display_usage ; exit ;;
     \?|*) display_usage
        exit 1;;
   esac
 done
-
-#define usage
-
-#test validity of inputs and run location
 
 #print out inputs to the log
 printf "Polygenic Risk Score Calculation Script
@@ -42,6 +66,19 @@ P-value thresholds: $pvalues
 R-squared threshold: $r2thresh
 Window size: $window
 "
+
+if [ "$apoe_exclude" == "yes" ];
+then 
+    printf "PRS will be calculated with and without the APOE region (using coordinates from genome build ${genome_build}).\n"
+fi
+
+#make sure the script is being run from the scripts directory
+if [ ! -f Determine_overlapping_SNPs.R ];
+then 
+    printf "The working directory does not contain the scripts! Please rerun from within the scripts directory for PRS calculation!\n"
+    exit 1
+fi
+
 
 #get genotypes_stem (sans path) using shell magic
 genotypes_stem=${genotypes##*/}
@@ -68,8 +105,8 @@ current output tag: $output_tag_current\n"
     #Extract overlapping SNPs from the genotype data
     plink --bfile $genotypes --allow-no-sex --extract ${output_folder}/${output_tag_current}_overlapping_SNPs.txt --make-bed --out ${output_folder}/${genotypes_stem}_${output_tag_current} > /dev/null 2>&1 
     genotypes_new=${output_folder}/${genotypes_stem}_${output_tag_current}
-
-    printf "Step 2: Performing LD clumping for Soutput_tag_current \n"
+    
+    printf "Step 2: Performing LD clumping for $output_tag_current \n"
     #Perform LD clumping
     plink --bfile ${genotypes_new} --allow-no-sex --clump ${output_folder}/${output_tag_current}_summary_stats_updated.txt --clump-p1 1 --clump-r2 $r2thresh --clump-kb $window --out ${genotypes_new}  > /dev/null 2>&1
     
@@ -83,11 +120,18 @@ current output tag: $output_tag_current\n"
     #Calculate PRS
     plink --bfile $genotypes_new --allow-no-sex --score ${genotypes_new}_score_input.txt --q-score-range ${output_folder}/${output_tag_current}_pvalue_range.txt ${genotypes_new}_score_input.txt 1 3 --out ${genotypes_new}_PRS  > /dev/null 2>&1
     PRS_stem=${genotypes_new}_PRS
-    
     #report the number of variants that were skipped, if any were skipped
     skipped_var=$( grep "lines skipped in --score" ${genotypes_new}_PRS.log | grep -o -E '[0-9]+' | head -n1 )
     if [ ! -z "$skipped_var" ]; then printf "$skipped_var variants skipped from PRS calculation. See ${genotypes_new}_PRS.log for more details.\n"; fi
-    
+
+    #if the no-apoe flag was specified, then recalculate the PRS without APOE
+    if [ "$apoe_exclude" == "yes" ];
+    then
+	printf "Calculating PRS for ${output_tag_current} without the APOE region\n"
+	#Calculate PRS
+	plink --bfile $genotypes_new --allow-no-sex --exclude range APOE_region_${genome_build}.txt --score ${genotypes_new}_score_input.txt --q-score-range ${output_folder}/${output_tag_current}_pvalue_range.txt ${genotypes_new}_score_input.txt 1 3 --out ${genotypes_new}_noAPOE_PRS  > /dev/null 2>&1
+    fi
+        
     #check for variants that failed to be incorporated into the score
     if [ -f  ${PRS_stem}.nopred ];
     then
@@ -104,6 +148,17 @@ current output tag: $output_tag_current\n"
 	#report the number of variants that were skipped, if any were skipped
 	skipped_var=$( grep "lines skipped in --score" ${genotypes_new}_PRS.log | grep -o -E '[0-9]+' | head -n1 )
 	if [ ! -z "$skipped_var" ]; then printf "$skipped_var variants skipped from PRS calculation. See ${genotypes_new}_PRS.log for more details.\n"; fi
+
+
+	#if the no-apoe flag was specified, then recalculate the PRS without APOE
+	if [ "$apoe_exclude" == "yes" ];
+	then
+	    printf "Recalculating the PRS excluding the APOE region.\n"
+            #Calculate PRS
+            plink --bfile ${genotypes_new}_flipsnps --allow-no-sex --exclude range APOE_region_${genome_build}.txt --score ${genotypes_new}_score_input.txt --q-score-range ${output_folder}/${output_tag_current}_pvalue_range.txt ${genotypes_new}_score_input.txt 1 3 --out ${genotypes_new}_noAPOE_PRS  > /dev/null 2>&1
+	fi
+
+
     fi
 
 #Clean-up
@@ -114,8 +169,8 @@ done
 printf "Step 4: Combining PRS into one file\n"
 
 #combine all PRS into one file
-Rscript Compile_PRS.R $output_folder $genotypes_stem $output_tags $pvalues $output
+Rscript Compile_PRS.R $output_folder $genotypes_stem $output_tags $pvalues $output $apoe_exclude
 
-printf "Step5: Generating correlation plot of all generated scores\n"
+printf "Step 5: Generating correlation plot of all generated scores\n"
 
 Rscript Create_PRS_corplot.R ${output_folder}/${output}.txt
