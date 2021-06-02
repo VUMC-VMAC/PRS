3abaef618a61fc3f69bd9d1b9548ceed29d2124b
ermahoney18 <emily.mahoney@vumc.org>
2021-06-02 12:58:32 -0500

    add check for multi-allelic variants

--- Determine_overlapping_SNPs.R
+++ Determine_overlapping_SNPs.R
@@ -11,18 +11,47 @@ library(data.table)
 summary <- fread(sumstats, select = c("SNP", "A1", "A2", "BETA", "P"))
 
 # import Cohort SNPs
-genetic <- fread(paste0(genotypes,".bim"), select = c(2))
-names(genetic) <- c("SNP")
-
-# determine SNP overlap
-summary_genetic_overlap <- summary[summary$SNP %in% genetic$SNP,]
+genetic <- fread(paste0(genotypes,".bim"), select = c(2,5,6))
+names(genetic) <- c("SNP", "A1", "A2")
 
 # Remove ambiguous SNPs (ie, A/T and G/C SNPs)
-ambigSNPs <- summary_genetic_overlap$SNP[(summary_genetic_overlap$A1=="A" & summary_genetic_overlap$A2=="T") | 
-	     				 (summary_genetic_overlap$A1=="T" & summary_genetic_overlap$A2=="A") |
-					 (summary_genetic_overlap$A1=="G" & summary_genetic_overlap$A2=="C") |
-					 (summary_genetic_overlap$A1=="C" & summary_genetic_overlap$A2=="G")]
-summary_genetic_overlap <- summary_genetic_overlap[!summary_genetic_overlap$SNP %in% ambigSNPs,]
+ambigSNPs <- genetic$SNP[(genetic$A1=="A" & genetic$A2=="T") | 
+	     		 (genetic$A1=="T" & genetic$A2=="A") |
+			 (genetic$A1=="G" & genetic$A2=="C") |
+			 (genetic$A1=="C" & genetic$A2=="G")]
+genetic <- genetic[!genetic$SNP %in% ambigSNPs,]
+
+# determine overlap by merging on variant id, A1 and A2
+summary_genetic_overlap <- merge(summary, genetic, by = c("SNP", "A1", "A2"))
+
+#check for variants which are just on the opposite strand
+summary_genetic_overlap_oppallele <- merge(summary, genetic, by.x = c("SNP", "A1", "A2"), by.y = c("SNP", "A2", "A1"))
+summary_genetic_overlap <- rbind(summary_genetic_overlap, summary_genetic_overlap_oppallele)
+rm(summary_genetic_overlap_oppallele)
+
+#check for variants which have to be flipped
+genetic$A1_old <- genetic$A1
+genetic$A2_old <- genetic$A2
+genetic$A1[genetic$A1_old == "A"] <- "T"
+genetic$A1[genetic$A1_old == "T"] <- "A"
+genetic$A1[genetic$A1_old == "G"] <- "C"
+genetic$A1[genetic$A1_old == "C"] <- "G"
+genetic$A2[genetic$A2_old == "A"] <- "T"
+genetic$A2[genetic$A2_old == "T"] <- "A"
+genetic$A2[genetic$A2_old == "G"] <- "C"
+genetic$A2[genetic$A2_old == "C"] <- "G"
+genetic$A1_old <- NULL
+genetic$A2_old <- NULL
+summary_genetic_overlap_flip <- merge(summary[!summary$SNP %in% summary_genetic_overlap$SNP,], genetic, by = c("SNP", "A1", "A2"))
+summary_genetic_overlap_flip_oppallele <- merge(summary, genetic, by.x = c("SNP", "A1", "A2"), by.y = c("SNP", "A2", "A1"))
+summary_genetic_overlap_flip <- rbind(summary_genetic_overlap_flip, summary_genetic_overlap_flip_oppallele)
+rm(summary_genetic_overlap_flip_oppallele)
+
+#write out the list of those variants to flip at the front end
+write.table(summary_genetic_overlap_flip$SNP,  paste(PRS_tag,"_SNPs_to_flip.txt",sep=""),quote=FALSE,  row.names=FALSE, col.names=FALSE, sep=" ")
+
+#combine to keep all the variants which overlap and have matching (or flipped) alleles
+summary_genetic_overlap <- rbind(summary_genetic_overlap, summary_genetic_overlap_flip)
 
 # export updated summary file
 write.table(summary_genetic_overlap,  paste0(PRS_tag,"_summary_stats_updated.txt"),quote=FALSE, row.names=FALSE,  col.names=TRUE, sep=" ")
@@ -32,5 +61,5 @@ write.table(summary_genetic_overlap[,c("SNP")], paste(PRS_tag,"_overlapping_SNPs
 
 #print information to the log
 print(paste(format(nrow(summary), big.mark = ","), "variants present in", PRS_tag, "summary statistics."))
-print(paste(format(nrow(summary_genetic_overlap)+length(ambigSNPs), big.mark = ","), "variants overlap with", genotypes, "."))
+print(paste(format(nrow(summary_genetic_overlap)+length(ambigSNPs), big.mark = ","), "variants overlap with", genotypes, ", including", format(nrow(summary_genetic_overlap_flip), big.mark = ","), "variants which need alleles flipped."))
 print(paste(format(length(ambigSNPs), big.mark = ","), "ambiguous variants removed, leaving", format(nrow(summary_genetic_overlap), big.mark = ","), "for input to PRS calculation."))
